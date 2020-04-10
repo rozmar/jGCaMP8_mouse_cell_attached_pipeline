3f8e0e3ac161949b197ca959ab100b34cea48833
Ilya Kolb <kolbi@hhmi.org>
2020-04-10 11:27:23 -0400

    add f0, dff calculations to apsegment

--- apsegment.py
+++ apsegment.py
@@ -15,7 +15,11 @@ APsegment: isolated segment of F and V recording
 @author: kolbi
 """
 
+import numpy as np
+
 class APsegment(object):
+    
+    
     def __init__(self, batchDict, Fsegment, Vsegment, sRates, nAPs):
         
         self.batchDict = batchDict
@@ -25,4 +29,44 @@ class APsegment(object):
         self.sr_f = sRates['sr_f']
         self.nAPs = nAPs
         
+        # assumes response starts in the middle of the trace
+        self.mdpt = int(len(self.Fsegment)/2)
+        self.F0 = self.calcF0()
+        self.dFF = self.calcdFF()
         
+        (self.dFFpeak, self.dFFpeak_t_s) = self.dFFpeak()
+    
+    def calcF0(self):
+        """
+        calculates F0 of Fsegment
+        """
+        
+        # F0: Fsegment[mdpt-offset-dur:mdpt-offset]
+        offset = 5
+        dur = 5 # num samples to take
+        return np.mean(self.Fsegment[self.mdpt-offset-dur:self.mdpt-offset])
+    
+    def calcdFF(self):
+        """
+        return delta F / F0 trace
+        """
+        
+        return (self.Fsegment - self.F0) / self.F0
+    
+    def dFFpeak(self):
+        """
+        calculate peak dF/F of Fsegment
+        return dF/F_peak and t_dF/F_peak (time from mdpt to peak)
+        """
+        
+        afterMid = self.Fsegment[self.mdpt:]
+        max_idx_ = np.argmax(afterMid)
+        max_idx = max_idx_ + self.mdpt
+        
+        return (self.Fsegment[max_idx], max_idx_*self.sr_f)
+    
+    def isGood(self):
+        """
+        flag to determine whether the APsegment is good
+            e.g. noise too high?
+        """
--- scratch.py
+++ scratch.py
@@ -1,29 +1,17 @@
 import numpy as np
-import tifffile as tiff
 import matplotlib.pyplot as plt
 from apsegment import APsegment 
 
 # plot F traces
-bucket = np.load(r'F:\ufGCaMP2pData\Analysis\bucket_3AP.npy', allow_pickle=True)
+bucket = np.load(r'F:\ufGCaMP2pData\Analysis\bucket_2AP.npy', allow_pickle=True)
 
-allF = np.asarray([f.Fsegment for f in bucket])
+allF = np.asarray([f.dFF for f in bucket])
+allF = allF.T
 
-allF = allF.T - np.mean(allF, axis=1)
+# allF = allF.T - np.mean(allF, axis=1)
 meanF = np.mean(allF, axis=1)
 
 plt.figure()
 plt.plot(allF, color='0.8')
 plt.plot(meanF, 'k-')
-'''
-from polygondrawer import PolygonDrawer
 
-maxProj = tiff.imread(r'F:/ufGCaMP2pData/martonData/20200322-anm472004/cell5/suite2p/plane0/maxproj_chan0_cell5_stim03_.tif')
-
-scaledImg = (maxProj - np.min(maxProj)) / (np.max(maxProj) - np.min(maxProj))
-
-pd = PolygonDrawer("Polygon", scaledImg)
-mask = pd.run()
-
-mask = mask.astype(bool) # convert to boolean mask array
-
-'''
