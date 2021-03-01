f02e225c3921d565287e46db428ebb35375edfa8
rozmar <qtyush@gmail.com>
2021-03-01 14:02:45 -0500

    export movies for Jerome

--- utils/utils_export.py
+++ utils/utils_export.py
@@ -7,8 +7,183 @@ import matplotlib.pyplot as plt
 import shutil
 import pandas as pd
 from pathlib import Path
+import json
 from utils import utils_plot, utils_ephys
 #%%
+def export_movies_with_metadata():
+    #%% export traces for s2f
+    # this script locates the cells with highest snr for each sensor, then exports the first n cells or m movies, whichever comes first
+    sensors_to_export = ['686','688']
+    sensor_names = {'686':'jGCaMP8m',
+                    '688':'jGCaMP8s'}
+    #%
+    savedir = '/home/rozmar/Mount/HDD_RAID_2_16TB/GCaMP8_deepinterpolation_export'
+    roi_crit = {'channel_number':1,
+                'motion_correction_method':'Suite2P',
+                'roi_type':'Suite2P',
+                'neuropil_number':1}
+    max_sweep_ap_hw_std_per_median =  .2
+    min_sweep_ap_snr_dv_median = 10
+    #minimum_f0_value = 20 # when the neuropil is bright, the F0 can go negative after neuropil subtraction
+    min_channel_offset = 100 # PMT error
+    ephys_quality_crit_1 = 'sweep_ap_snr_dv_median>={}'.format(min_sweep_ap_snr_dv_median)
+    ephys_quality_crit_2 = 'sweep_ap_hw_std_per_median<={}'.format(max_sweep_ap_hw_std_per_median)
+    #baseline_f_crit = 'cawave_baseline_f > {}'.format(minimum_f0_value)
+    channel_offset_crit = 'channel_offset > {}'.format(min_channel_offset)
+    
+    calcium_sensors = np.unique(imaging_gt.SessionCalciumSensor().fetch('session_calcium_sensor'))
+    max_cells_to_export = 200 # per sensor
+    max_movies_to_export = 2000 #per sensor
+    #%
+    for sensor in calcium_sensors:
+        if sensor not in sensors_to_export:
+            continue
+        
+        sensor_crit = 'session_calcium_sensor="{}"'.format(sensor)
+        movies_all = (imaging.MovieChannel()*ephysanal_cell_attached.SweepAPQC()*imaging_gt.SessionCalciumSensor()*imaging_gt.MovieCalciumWaveSNR() & sensor_crit&ephys_quality_crit_1&ephys_quality_crit_2&channel_offset_crit)
+        subject_ids = np.unique(movies_all.fetch('subject_id'))
+
+    #%
+        median_cell_snr_list = list()
+        subject_id_list = list()
+        cell_number_list = list()
+        movie_number_list = list()
+        cell_total_movie_len_list = list()
+        cell_total_ap_num_list = list()
+        for subject_id in subject_ids:
+            subject_crit = 'subject_id = {}'.format(subject_id)
+            cell_numbers = np.unique((movies_all*imaging_gt.ROISweepCorrespondance() & subject_crit).fetch('cell_number'))
+            for cell_number in cell_numbers:
+                cell_crit = 'cell_number = {}'.format(cell_number)
+                cell_snrs,cell_movie_total_ap_num,sweep_start_time,sweep_end_time = (movies_all*imaging_gt.ROISweepCorrespondance()*ephys_cell_attached.Sweep()&subject_crit&cell_crit).fetch('movie_median_cawave_snr_per_ap','movie_total_ap_num','sweep_start_time','sweep_end_time')
+                cell_movie_len = np.asarray(sweep_end_time-sweep_start_time,float)
+                #cell_firing_rate = cell_movie_total_ap_num/cell_movie_len
+                median_cell_snr_list.append(np.median(cell_snrs))
+                movie_number_list.append(len(cell_snrs))
+                subject_id_list.append(np.median(subject_id))
+                cell_number_list.append(np.median(cell_number))
+                cell_total_movie_len_list.append(np.sum(cell_movie_len))
+                cell_total_ap_num_list.append(np.sum(cell_movie_total_ap_num))
+         #%
+        cell_number_list=np.asarray(cell_number_list)
+        subject_id_list=np.asarray(subject_id_list)
+        median_cell_snr_list=np.asarray(median_cell_snr_list)
+        movie_number_list=np.asarray(movie_number_list)
+        cell_total_movie_len_list=np.asarray(cell_total_movie_len_list)
+        cell_total_ap_num_list=np.asarray(cell_total_ap_num_list)
+        
+        order = np.argsort(median_cell_snr_list)
+        
+        
+        median_cell_snr_list = median_cell_snr_list[order][::-1]
+        subject_id_list = subject_id_list[order][::-1]
+        cell_number_list = cell_number_list[order][::-1]
+        movie_number_list = movie_number_list[order][::-1]
+        cell_total_movie_len_list = cell_total_movie_len_list[order][::-1]
+        cell_total_ap_num_list = cell_total_ap_num_list[order][::-1]
+        
+        cell_counter = 0
+        movie_counter = 0
+        #%
+        for subject_id,cell_number,median_cell_snr,total_movie_len,total_ap_num in zip(subject_id_list,cell_number_list,median_cell_snr_list,cell_total_movie_len_list,cell_total_ap_num_list):
+            if cell_counter>max_cells_to_export or movie_counter > max_movies_to_export:
+                break
+            cell_counter+=1
+            #%
+            subject_crit = 'subject_id = {}'.format(subject_id)
+            cell_crit = 'cell_number = {}'.format(cell_number)
+            celltype = (ephysanal_cell_attached.EphysCellType()& subject_crit & cell_crit).fetch1('ephys_cell_type')
+            
+            #%
+            cell_movies = movies_all*imaging_gt.ROISweepCorrespondance() & subject_crit & cell_crit# &'sweep_number = 2'
+            snrs,movie_numbers = cell_movies.fetch('movie_median_cawave_snr_per_ap','movie_number')
+            order = np.argsort(snrs)[::-1]
+            snrs = snrs[order]
+            movie_numbers = movie_numbers[order]
+            
+            cell_metadata_dict = {'median_cell_snr':median_cell_snr,
+                                  'putative_celltype':celltype,
+                                  'total_ap_num':total_ap_num,
+                                  'total_movie_lenght':total_movie_len,
+                                  'movie_number':len(movie_numbers)
+                                  }
+            #%
+            savedir_cell_movie = Path(os.path.join(savedir,'movies',sensor_names[sensor],'anm{}_cell_{}'.format(int(subject_id),int(cell_number))))
+            savedir_cell_metadata = Path(os.path.join(savedir,'metadata',sensor_names[sensor],'anm{}_cell_{}'.format(int(subject_id),int(cell_number))))
+            savedir_cell_metadata.mkdir(parents=True, exist_ok=True)
+            with open(savedir_cell_metadata/'cell_metadata.json', 'w') as outfile:
+                json.dump(cell_metadata_dict, outfile, indent=4)
+            #%
+            for movie_number, snr in zip(movie_numbers,snrs):
+                metadata_files_alread_saved = os.listdir(savedir_cell_metadata)
+                dj.conn().connect()
+                savedir_movie = savedir_cell_movie/'movie_{}'.format(int(movie_number))
+                savedir_movie.mkdir(parents=True, exist_ok=True)
+                #break
+            #%
+                try:
+                    metadata_filename = 'movie_{}.json'.format(int(movie_number))
+                    if metadata_filename in metadata_files_alread_saved:
+                        print('{} already saved'.format(metadata_filename))
+                        continue
+                    else:
+                        print('exporting to {}'.format(savedir_movie))
+                        
+                        
+                        
+                        
+                     #%
+                    movie_crit = 'movie_number = {}'.format(movie_number)
+                    big_table = imaging.Movie()*ephys_cell_attached.SweepMetadata()*imaging.ROI()*imaging.ROITrace()*imaging.ROINeuropilTrace()*imaging.MovieFrameTimes()*cell_movies&movie_crit&roi_crit#*ephysanal_cell_attached.ActionPotential()
+                    #%
+                    roi_centroid_x, roi_centroid_y, frame_times,roi_time_offset,movie_x_size,movie_y_size,movie_frame_num,movie_name,movie_pixel_size = big_table.fetch1('roi_centroid_x','roi_centroid_y','frame_times','roi_time_offset','movie_x_size','movie_y_size','movie_frame_num','movie_name','movie_pixel_size') #ap_times
+                    ap_max_times = (ephysanal_cell_attached.ActionPotential()*cell_movies&movie_crit&roi_crit).fetch('ap_max_time')
+                    cell_recording_start, session_time = (ephys_cell_attached.Cell()*experiment.Session()*cell_movies&movie_crit).fetch1('cell_recording_start', 'session_time')
+                    
+                    sweep_start_time =  (ephys_cell_attached.Sweep()*cell_movies&movie_crit).fetch1('sweep_start_time')
+                    sample_rate = (ephys_cell_attached.SweepMetadata()*ephys_cell_attached.SweepResponse()*cell_movies&movie_crit).fetch1('sample_rate')
+    
+                    frame_times = frame_times - ((cell_recording_start-session_time).total_seconds()+float(sweep_start_time))
+                    #%
+                    movie_files = imaging.RegisteredMovieFile()*cell_movies&movie_crit
+                    reg_movie_file_repository,reg_movie_file_directory,reg_movie_file_name = movie_files.fetch('reg_movie_file_repository','reg_movie_file_directory','reg_movie_file_name')
+                    for reg_movie_file_repository_now,reg_movie_file_directory_now,reg_movie_file_name_now in zip(reg_movie_file_repository,reg_movie_file_directory,reg_movie_file_name):
+                        source_dir = os.path.join(dj.config['locations.{}'.format(reg_movie_file_repository_now)],reg_movie_file_directory_now)
+                        destination_dir = savedir_movie
+                        os.system('rsync -rv --size-only '+ '\'' + str(source_dir) +'/'+ '\'' +' '+ '\''+str(destination_dir)+ '\''+'/')
+                       
+                        break
+                        source_file = os.path.join(dj.config['locations.{}'.format(reg_movie_file_repository_now)],reg_movie_file_directory_now,reg_movie_file_name_now)
+                        destination_file = os.path.join(savedir_movie,reg_movie_file_name_now)
+                        shutil.copyfile(source_file, destination_file)
+                        
+                    #%
+                    movie_metadata_dict = {'roi_centroid_x':float(roi_centroid_x),
+                                           'roi_centroid_y':float(roi_centroid_y),
+                                           'ephys_sample_rate':int(sample_rate),
+                                           'movie_x_size':int(movie_x_size),
+                                           'movie_y_size':int(movie_y_size),
+                                           'movie_pixel_size':float(movie_pixel_size),
+                                           'movie_frame_num':int(movie_frame_num),
+                                           'movie_name':movie_name,
+                                           'roi_time_offset':round(roi_time_offset,4),
+                                           'movie_1ap_snr':round(snr,1),
+                                           'ap_max_times':list(np.asarray(ap_max_times,float)),
+                                           'frame_start_times':list(np.round(np.asarray(frame_times,float),4)),
+                                           }
+                    
+                    with open(savedir_cell_metadata/'movie_{}.json'.format(movie_number), 'w') as outfile:
+                        json.dump(movie_metadata_dict, outfile, indent=4)
+                    #%
+                except:
+                    print('error with this record')
+                    cell_movies&movie_crit&roi_crit
+   
+#%%
+
+
+
+
 def export_s2f_data_from_datajoint():
     #%% export traces for s2f
     # this script locates the cells with highest snr for each sensor, then exports the first n cells or m movies, whichever comes first
